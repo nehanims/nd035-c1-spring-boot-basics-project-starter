a0c2c176cf3afbe7f735bf410163b64140e89c31
Neha <neha.h.nims@gmail.com>
2021-04-02 18:05:53 -0700

    -adds file download functionality -adds file delete functionality

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileManagementController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileManagementController.java
@@ -1,11 +1,18 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.mapper.Files;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileManagementService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,5 +32,31 @@ public class FileManagementController {
     public String uploadFile(@RequestParam("fileUpload") MultipartFile multipartFile, Authentication authentication, Model model){
         fileService.uploadFile(multipartFile, userService.getLoggedInUserId(authentication));
         return "redirect:/home";
+    }
+
+    @GetMapping("/delete-file/{id}")
+    public String deleteFile(@PathVariable("id") Integer fileId) {
+        //TODO before deleting or updating check if the current logged in user has the right to send the request, i.e delete this file,
+        // in fact check this for all types of updates and deletes,
+        // you could add a check in the update and delete for each file, note and credential query itself i.e. add to the where clause userid=loggedinuserid,
+        // and then if no rows are updated or deleted then return failure response up through the service layer to the controller
+
+        //TODO don't allow upload of existing filename,
+        //TODO don't allow upload of empty file
+        //TODO how to handle multiple file upload?
+
+        fileService.deleteFile(fileId);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/view-file/{id}")//TODO change these end point mappings to be more RESTful e.g. /file/view/{fileId}
+    public ResponseEntity<Resource> getFile ( @PathVariable("id") Long fileId, Authentication authentication)
+    {
+        Files file = fileService.getFileByFileId(fileId);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(file.getContentType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""
+                + file.getFilename() + "\"").body(new
+                        ByteArrayResource(file.getFileData()));
     }
 }
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FileMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FileMapper.java
@@ -1,14 +1,14 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface FileMapper {
+
+    @Select("SELECT * FROM Files WHERE fileid=#{fileId}")
+    Files getFile(Long fileId);
 
     @Select("SELECT * FROM Files WHERE userid = #{loggedInUserId}")
     List<Files> getAllFiles(Integer loggedInUserId);
@@ -16,5 +16,8 @@ public interface FileMapper {
     @Insert("INSERT INTO Files (filename, contenttype, filesize, userid, filedata) VALUES (#{filename}, #{contentType}, #{fileSize}, #{userId}, #{fileData})")
     @Options(useGeneratedKeys = true, keyProperty = "fileId")
     void saveFile(Files file);
+
+    @Delete("DELETE FROM Files WHERE fileid=#{fileId}")
+    void deleteFile(Integer fileId);
 
 }
